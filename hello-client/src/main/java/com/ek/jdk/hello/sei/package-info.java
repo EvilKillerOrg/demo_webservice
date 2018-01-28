ef2ca7e999ee2dc01f9c7c15fbc202bf37cd5e79
EK-T520 <whm029@126.com>
2018-01-28 22:02:32 +0800

    rebuild project and add weather client

---
+++ hello-client/src/main/java/com/ek/jdk/hello/sei/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://sei.hello.jdk.ek.com/")
+package com.ek.jdk.hello.sei;
--- w-client/src/main/java/com/ek/jdk/service/sei/impl/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://impl.sei.service.jdk.ek.com/")
-package com.ek.jdk.service.sei.impl;
