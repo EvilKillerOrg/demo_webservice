578251c7e7f4424abe2ebdc6bc4bb72ab2a66c69
EK-T520 <whm029@126.com>
2018-01-28 19:59:17 +0800

    rebuild project

---
+++ w-client/src/main/java/com/ek/jdk/service/sei/impl/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://impl.sei.service.jdk.ek.com/")
+package com.ek.jdk.service.sei.impl;
---
+++ w-service/src/main/java/com/ek/jdk/service/sei/impl/HelloWSImpl.java
@@ -0,0 +1,29 @@
+package com.ek.jdk.service.sei.impl;
+
+
+import javax.jws.WebService;
+import org.apache.log4j.Logger;
+import com.ek.jdk.service.sei.IHelloWS;
+
+/**
+ * 这是SEI的实现
+ * 
+ * @ClassName: HelloWSImpl
+ * @Description: TODO
+ * @author: ek
+ * @date: 2018年1月28日 上午4:40:12
+ */
+
+@WebService // SEI的实现类也要给一个@WebService注解,方法不用加注解了
+public class HelloWSImpl implements IHelloWS {
+
+  private Logger log = Logger.getLogger(HelloWSImpl.class);
+  
+  @Override
+  public String sayHello(String name) {
+    log.info("Is Service Endpoint Interface Print "+name);
+    
+    return "Hello " + name;
+  }
+
+}
